d932ad55aac1ae46923594fddc0a79bf32a47277
Greg Alboum <alboumgr@msu.edu>
2025-08-31 12:28:19 -0400

    updates

--- plt-dapp/plt_dapp_v2/frontend/vite.config.js
+++ plt-dapp/plt_dapp_v2/frontend/vite.config.js
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',   // 👈 ensures relative paths for assets
   server: {
     host: true, // This tells Vite to listen on all network interfaces
     port: 5173, // Optional: Explicitly set the port, though 5173 is default
