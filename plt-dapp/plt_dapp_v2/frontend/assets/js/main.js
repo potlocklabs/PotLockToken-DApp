dcdbf24526ca24518cad50626cc75b00a19d9e6e
Greg Alboum <alboumgr@msu.edu>
2025-09-01 19:19:16 -0400

    updates

--- plt-dapp/plt_dapp_v2/frontend/assets/js/main.js
+++ plt-dapp/plt_dapp_v2/frontend/assets/js/main.js
@@ -4,135 +4,123 @@ import './util.js';
 import './animations.js';
 import './token_interaction.js';
 import './wallet_connect_handler.js';
-import './breakpoints.min.js';
 
-// Wait for the entire page to be fully loaded, including all scripts.
-window.onload = function() {
+var breakpoints=function(){"use strict";function e(e){t.init(e)}var t={list:null,media:{},events:[],init:function(e){t.list=e,window.addEventListener("resize",t.poll),window.addEventListener("orientationchange",t.poll),window.addEventListener("load",t.poll),window.addEventListener("fullscreenchange",t.poll)},active:function(e){var n,a,s,i,r,d,c;if(!(e in t.media)){if(">="==e.substr(0,2)?(a="gte",n=e.substr(2)):"<="==e.substr(0,2)?(a="lte",n=e.substr(2)):">"==e.substr(0,1)?(a="gt",n=e.substr(1)):"<"==e.substr(0,1)?(a="lt",n=e.substr(1)):"!"==e.substr(0,1)?(a="not",n=e.substr(1)):(a="eq",n=e),n&&n in t.list)if(i=t.list[n],Array.isArray(i)){if(r=parseInt(i[0]),d=parseInt(i[1]),isNaN(r)){if(isNaN(d))return;c=i[1].substr(String(d).length)}else c=i[0].substr(String(r).length);if(isNaN(r))switch(a){case"gte":s="screen";break;case"lte":s="screen and (max-width: "+d+c+")";break;case"gt":s="screen and (min-width: "+(d+1)+c+")";break;case"lt":s="screen and (max-width: -1px)";break;case"not":s="screen and (min-width: "+(d+1)+c+")";break;default:s="screen and (max-width: "+d+c+")"}else if(isNaN(d))switch(a){case"gte":s="screen and (min-width: "+r+c+")";break;case"lte":s="screen";break;case"gt":s="screen and (max-width: -1px)";break;case"lt":s="screen and (max-width: "+(r-1)+c+")";break;case"not":s="screen and (max-width: "+(r-1)+c+")";break;default:s="screen and (min-width: "+r+c+")"}else switch(a){case"gte":s="screen and (min-width: "+r+c+")";break;case"lte":s="screen and (max-width: "+d+c+")";break;case"gt":s="screen and (min-width: "+(d+1)+c+")";break;case"lt":s="screen and (max-width: "+(r-1)+c+")";break;case"not":s="screen and (max-width: "+(r-1)+c+"), screen and (min-width: "+(d+1)+c+")";break;default:s="screen and (min-width: "+r+c+") and (max-width: "+d+c+")"}}else s="("==i.charAt(0)?"screen and "+i:i;t.media[e]=!!s&&s}return t.media[e]!==!1&&window.matchMedia(t.media[e]).matches},on:function(e,n){t.events.push({query:e,handler:n,state:!1}),t.active(e)&&n()},poll:function(){var e,n;for(e=0;e<t.events.length;e++)n=t.events[e],t.active(n.query)?n.state||(n.state=!0,n.handler()):n.state&&(n.state=!1)}};return e._=t,e.on=function(e,n){t.on(e,n)},e.active=function(e){return t.active(e)},e}();!function(e,t){"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?module.exports=t():e.breakpoints=t()}(this,function(){return breakpoints});
 
-    // Now it's guaranteed that the breakpoints function has been defined.
-    const breakpoints = window.breakpoints;
+(function($) {
 
-    // Use a safety check to ensure breakpoints is a function before calling it.
-    if (typeof breakpoints !== 'function') {
-      console.error("Error: breakpoints function is not defined. The library might not be loading correctly.");
-      return;
-    }
+	var	$window = $(window),
+		$body = $('body'),
+		$main = $('#main');
 
-    (function($) {
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
 
-        var $window = $(window),
-            $body = $('body'),
-            $main = $('#main');
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
-        // Breakpoints.
-        breakpoints({
-            xlarge:   [ '1281px',   '1680px' ],
-            large:    [ '981px',   '1280px' ],
-            medium:   [ '737px',   '980px'  ],
-            small:    [ '481px',   '736px'  ],
-            xsmall:   [ '361px',   '480px'  ],
-            xxsmall:  [ null,      '360px'  ]
-        });
+	// Nav.
+		var $nav = $('#nav');
 
-        // Play initial animations on page load.
-        $window.on('load', function() {
-            window.setTimeout(function() {
-                $body.removeClass('is-preload');
-            }, 100);
-        });
+		if ($nav.length > 0) {
 
-        // Nav.
-        var $nav = $('#nav');
+			// Shrink effect.
+				$main
+					.scrollex({
+						mode: 'top',
+						enter: function() {
+							$nav.addClass('alt');
+						},
+						leave: function() {
+							$nav.removeClass('alt');
+						},
+					});
 
-        if ($nav.length > 0) {
+			// Links.
+				var $nav_a = $nav.find('a');
 
-            // Shrink effect.
-            $main
-                .scrollex({
-                    mode: 'top',
-                    enter: function() {
-                        $nav.addClass('alt');
-                    },
-                    leave: function() {
-                        $nav.removeClass('alt');
-                    },
-                });
+				$nav_a
+					.scrolly({
+						speed: 1000,
+						offset: function() { return $nav.height(); }
+					})
+					.on('click', function() {
 
-            // Links.
-            var $nav_a = $nav.find('a');
+						var $this = $(this);
 
-            $nav_a
-                .scrolly({
-                    speed: 1000,
-                    offset: function() { return $nav.height(); }
-                })
-                .on('click', function() {
+						// External link? Bail.
+							if ($this.attr('href').charAt(0) != '#')
+								return;
 
-                    var $this = $(this);
+						// Deactivate all links.
+							$nav_a
+								.removeClass('active')
+								.removeClass('active-locked');
 
-                    // External link? Bail.
-                    if ($this.attr('href').charAt(0) != '#')
-                        return;
+						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+							$this
+								.addClass('active')
+								.addClass('active-locked');
 
-                    // Deactivate all links.
-                    $nav_a
-                        .removeClass('active')
-                        .removeClass('active-locked');
+					})
+					.each(function() {
 
-                    // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-                    $this
-                        .addClass('active')
-                        .addClass('active-locked');
+						var	$this = $(this),
+							id = $this.attr('href'),
+							$section = $(id);
 
-                })
-                .each(function() {
+						// No section for this link? Bail.
+							if ($section.length < 1)
+								return;
 
-                    var $this = $(this),
-                        id = $this.attr('href'),
-                        $section = $(id);
+						// Scrollex.
+							$section.scrollex({
+								mode: 'middle',
+								initialize: function() {
 
-                    // No section for this link? Bail.
-                    if ($section.length < 1)
-                        return;
+									// Deactivate section.
+										if (browser.canUse('transition'))
+											$section.addClass('inactive');
 
-                    // Scrollex.
-                    $section.scrollex({
-                        mode: 'middle',
-                        initialize: function() {
+								},
+								enter: function() {
 
-                            // Deactivate section.
-                            if (browser.canUse('transition'))
-                                $section.addClass('inactive');
+									// Activate section.
+										$section.removeClass('inactive');
 
-                        },
-                        enter: function() {
+									// No locked links? Deactivate all links and activate this section's one.
+										if ($nav_a.filter('.active-locked').length == 0) {
 
-                            // Activate section.
-                            $section.removeClass('inactive');
+											$nav_a.removeClass('active');
+											$this.addClass('active');
 
-                            // No locked links? Deactivate all links and activate this section's one.
-                            if ($nav_a.filter('.active-locked').length == 0) {
+										}
 
-                                $nav_a.removeClass('active');
-                                $this.addClass('active');
+									// Otherwise, if this section's link is the one that's locked, unlock it.
+										else if ($this.hasClass('active-locked'))
+											$this.removeClass('active-locked');
 
-                            }
+								}
+							});
 
-                            // Otherwise, if this section's link is the one that's locked, unlock it.
-                            else if ($this.hasClass('active-locked'))
-                                $this.removeClass('active-locked');
+					});
 
-                        }
-                    });
+		}
 
-                });
+	// Scrolly.
+		$('.scrolly').scrolly({
+			speed: 1000
+		});
 
-        }
-
-        // Scrolly.
-        $('.scrolly').scrolly({
-            speed: 1000
-        });
-
-    })(jQuery);
-};
+})(jQuery);
